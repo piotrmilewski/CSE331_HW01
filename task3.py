299a627c7f3f3e426a3d0cc4824e33879bbf6967
Piotr Milewski <piotr250tiger@gmail.com>
2020-09-16 15:13:38 -0400

    finished task 3

---
+++ task3.py
@@ -0,0 +1,50 @@
+import sys
+import numpy as np
+from scipy.stats import chisquare
+from task2 import decrypt
+
+
+def freqAnalysis(arr):
+    englishFreq = np.array([8.2, 1.5, 2.8, 4.3, 13, 2.2, 2, 6.1, 7, 0.15, 0.77, 4, 2.4, 6.7, 7.5, 1.9, 0.095, 6, 6.3,
+                            9.1, 2.8, 0.98, 2.4, 0.15, 2, 0.074])
+    lowestStat = sys.maxsize
+    alphaIndex = 0
+    for j in range(26):
+        ceaserArr = np.roll(arr, -j)
+        stat = chisquare(ceaserArr, englishFreq)[0]
+        if stat < lowestStat:
+            alphaIndex = j
+            lowestStat = stat
+    return chr(alphaIndex+65)
+
+
+ciphertext = input()
+ciphertextUpper = ciphertext.upper()
+cipherKeyLen = int(input())
+key = ""
+
+freqArray = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+currKeyIndex = 0
+while currKeyIndex < cipherKeyLen:
+    i = 0
+    keyIndex = 0
+    length = 0
+    while i < len(ciphertext):
+        currChar = ord(ciphertextUpper[i])
+        if 91 > currChar > 64:
+            if keyIndex == currKeyIndex:
+                currChar = currChar - 65
+                freqArray[currChar] = freqArray[currChar] + 1
+                length = length + 1
+            keyIndex = keyIndex + 1
+            if keyIndex == cipherKeyLen:
+                keyIndex = 0
+        i = i + 1
+    freqArray = freqArray / length
+    freqArray = freqArray * 100
+    key = key + freqAnalysis(freqArray)
+    freqArray.fill(0)
+    currKeyIndex = currKeyIndex + 1
+
+print(key)
+print(decrypt(ciphertext, key))
